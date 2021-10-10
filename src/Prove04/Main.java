cb987ed0f2b397bf6a36aa0d8ef526b7b54b3571
topla <gilotmaxime1@gmail.com>
2021-10-09 21:01:17 -0400

    New line!

--- src/Prove04/Main.java
+++ src/Prove04/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Patrick");
         System.out.println("Hi there...");
+        System.out.println("Bye!");
 
     }
 
